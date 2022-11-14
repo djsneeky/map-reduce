0474d3efe6721d268bd82b8eb2d2f7caf8fa3ad2
David Furtwengler <djfurtweng@gmail.com>
2022-11-13 19:08:50 -0600

    Add cmake build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include <map>
-#include <queue>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <vector>
 
-#include <omp.h>
-#include <mpi.h>
-
-// OpenMP threads are readers, mappers, and reducers
+#include "MapReduce.h"
 
 int main() {
-    std::cout << "Map Reduce: " << num_procs << " procs, " << num_threads << " threads per proc\n";
+    int num_procs = 0;
+    int num_threads = 0;
+    std::cout << "Map Reduce: " << num_procs << " procs, " << num_threads << " threads per proc" << std::endl;
 
     // master process keeps list of files
 
@@ -22,43 +15,4 @@ int main() {
     // master process is notified when all processors have finished
 
     return 0;
-}
-
-// openmp function that runs on each proc
-void mapReduce()
-{
-    std::map<std::string, int> word_map;
-    std::queue<std::string> line_q;     // one for each map thread
-
-    // reader thread requests file when no remaining work
-
-    // reader thread reads files and put lines of file into queues
-
-    // map thread will 'map' words into a local word_map
-    // for words in line, word_map[word]++
-
-    // map words to a queue for reducers
-
-    // reducer receives pair from queue and updates reducer_map
-}
-
-// function to read file and add lines to queue
-void populateLineQueue(std)
-{
-    
-}
-
-/**
- * @brief 
- * 
- * @param line 
- * @param word_map 
- */
-void populateWordMap(const std::string& line, std::map<std::string, int>& word_map, char delim)
-{
-    std::istringstream iss(line);
-    std::string word;
-    while (std::getline(iss, word, delim)) {
-        word_map[word]++;
-    }
 }
