c4d0a6448ac6a9717ce40454f5f23ba8b2b5d1f2
David Furtwengler <djfurtweng@gmail.com>
2022-11-26 17:43:20 -0600

    Add structure for parallel map reduce

--- src/MapReduce.cpp
+++ src/MapReduce.cpp
@@ -8,22 +8,38 @@
 #include <cstdbool>
 #include <iostream>
 
+#include <omp.h>
+
 // openmp function that runs on each proc
 void mapReduceParallel()
 {
-    // reader thread reads files and put lines of file into queues
-    // reader thread requests file when no remaining work
+    // initialize queues for lines - one for each mapper thread
+    std::vector<std::queue<std::string>> lineQueues;
 
-    // map thread will 'map' words into a local word map
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            // reader thread reads files and put lines of file into queues
+            // reader thread requests file when no remaining work
+        }
 
-    // map thread will put pairs onto individual queues for for each reducer
+        #pragma omp single
+        {
+            // map thread to read line queues and place words into thread local word map
+            // map map thread will put pairs onto queues for for each reducer
+        }
 
-    // reducer receives pair from queue and updates its map
+        #pragma omp single
+        {
+            // reducer thread receives pair from queue and updates its map
+        }
+    }
 }
 
 /**
  * @brief A serial implementation of the map reduce algorithm
- * 
+ *
  * @return true if run successfully, false otherwise
  */
 bool mapReduceSerial()
@@ -36,81 +52,87 @@ bool mapReduceSerial()
     // read file and put lines into a queue
     std::queue<std::string> lineQueue;
     std::map<std::string, int> wordMap;
-    if (!populateLineQueue("../test/files/jungle.txt", lineQueue)) {
+    if (!populateLineQueue("../test/files/jungle.txt", lineQueue))
+    {
         return false;
     }
 
     // read lines out of queue and populate a map
-    while (!lineQueue.empty()) {
+    while (!lineQueue.empty())
+    {
         populateWordMap(lineQueue.front(), wordMap, ' ');
         lineQueue.pop();
     }
-    
+
     // split words in map to other 'reducer' queues
     unsigned int reducerQueueId;
     std::map<std::string, int>::iterator it;
     for (it = wordMap.begin(); it != wordMap.end(); it++)
     {
-        reducerQueueId = wordHashFn(it->first) % maxReducers;
+        reducerQueueId = getReducerQueueId(it->first, wordHashFn, maxReducers);
     }
-    
+
     return true;
 }
 
 /**
  * @brief Takes a word and a hash function and returns its desired reducer queue destination
- * 
+ *
  * @param wordMap a reference to the word map to reduce
  * @param wordHashFn a reference to the hash function
  * @param maxReducers the max number of reducers used
- * 
+ *
  * @return the reducer queue id number
  */
-int getReducerQueueId(const std::string& word, const std::hash<std::string>& wordHashFn, int maxReducers)
+unsigned int getReducerQueueId(const std::string &word, const std::hash<std::string> &wordHashFn, int maxReducers)
 {
-    int num = (int) wordHashFn(word) % maxReducers;
+    unsigned int num = wordHashFn(word) % maxReducers;
 
     return num;
 }
 
 /**
  * @brief Reads a file line by line and populates a line queue
- * 
+ *
  * Used by the reader threads
- * 
+ *
  * @param fileName the absolute path of the file
  * @return true if the file has been processed successfully, false otherwise
  */
-bool populateLineQueue(const std::string& fileName, std::queue<std::string>& lineQueue)
+bool populateLineQueue(const std::string &fileName, std::queue<std::string> &lineQueue)
 {
     std::ifstream file(fileName);
-    if (file.is_open()) {
+    if (file.is_open())
+    {
         std::string line;
-        while (std::getline(file, line)) {
+        while (std::getline(file, line))
+        {
             lineQueue.push(line);
         }
         file.close();
         return true;
     }
-    else {
+    else
+    {
         return false;
     }
 }
 
 /**
  * @brief Takes a line and puts words into a map
- * 
+ *
  * Used by the mapper threads
- * 
+ *
  * @param line a string to process
  * @param word_map a reference to the map to populate
  * @param delim the delimiter used to find word boundaries
  */
-void populateWordMap(const std::string& line, std::map<std::string, int>& wordMap, char delim)
+void populateWordMap(const std::string &line, std::map<std::string, int> &wordMap, char delim)
 {
     std::istringstream iss(line);
     std::string word;
-    while (std::getline(iss, word, delim)) {
+    while (std::getline(iss, word, delim))
+    {
         wordMap[word]++;
     }
 }
